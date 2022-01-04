d8c832eb94da409bc424503c34b8f62c08493fd7
yang1318 <yang1318@naver.com>
2022-01-04 16:48:43 +0900

    유저리스트 제작중

--- app/src/main/java/com/example/chat_de/ChatUserListAcitivity.java
+++ app/src/main/java/com/example/chat_de/ChatUserListAcitivity.java
@@ -3,15 +3,40 @@ package com.example.chat_de;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 public class ChatUserListAcitivity extends AppCompatActivity {
-
+    String[] items = {"전체","1-10기","11-20기","21-30기","31-40기","41-50기","51-60기","61기-70기","71기~"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_user_list);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item,items
+        );
+        //항목 선택시 보이는 별도창의 각 아이템을 위한 레이아웃 설정
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            //아이템이 선택되면
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                // int i : item의 순서대로 0번부터 n-1번까지
+            }
+            //아무것도 선택되지 않은 상태일때
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
     private ArrayList<FirebaseDataStructure.User> getAllUserList(){

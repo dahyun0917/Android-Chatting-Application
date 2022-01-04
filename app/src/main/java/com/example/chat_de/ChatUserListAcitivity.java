be2f1687e1b0f83bf358ae602f9fd52059fcd487
yang1318 <yang1318@naver.com>
2022-01-04 11:40:09 +0900

    기본틀

---
+++ app/src/main/java/com/example/chat_de/ChatUserListAcitivity.java
@@ -0,0 +1,14 @@
+package com.example.chat_de;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatUserListAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_user_list);
+    }
+}

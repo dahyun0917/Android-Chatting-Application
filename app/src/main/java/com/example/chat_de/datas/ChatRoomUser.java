b2f2a143ac7874baa37d90941e3f1aa5d1b3d1d6
mokiia <mokiia0325@gmail.com>
2022-01-05 11:21:15 +0900

    ChatRoomUser, UserMeta에 userKey field 추가함

--- app/src/main/java/com/example/chat_de/datas/ChatRoomUser.java
+++ app/src/main/java/com/example/chat_de/datas/ChatRoomUser.java
@@ -5,13 +5,17 @@ import com.google.firebase.database.IgnoreExtraProperties;
 @IgnoreExtraProperties
 public class ChatRoomUser {
     private int lastReadIndex;
+    private String userKey;
 
     public ChatRoomUser() { }
-    public ChatRoomUser(int lastReadIndex) {
+    public ChatRoomUser(int lastReadIndex, String userKey) {
         setLastReadIndex(lastReadIndex);
+        setUserKey(userKey);
     }
 
-    public int getLastReadIndex() { return lastReadIndex; }
+    public int getLastReadIndex()   { return lastReadIndex; }
+    public String getUserKey()      { return userKey; }
 
     public void setLastReadIndex(int lastReadIndex) { this.lastReadIndex = lastReadIndex; }
+    public void setUserKey(String userKey)          { this.userKey = userKey; }
 }

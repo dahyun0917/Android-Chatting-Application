6d4e27793f6ba03c583cc6de121606f9ed6c9998
yang1318 <yang1318@naver.com>
2022-01-05 10:55:16 +0900

    기수정보 추가 + 유저리스트 제작중2

--- app/src/main/java/com/example/chat_de/datas/UserMeta.java
+++ app/src/main/java/com/example/chat_de/datas/UserMeta.java
@@ -6,16 +6,20 @@ import com.google.firebase.database.IgnoreExtraProperties;
 public class UserMeta {
     private String name;
     private String pictureURL;
+    private int generation;
 
     public UserMeta() { }
-    public UserMeta(String name, String pictureURL) {
+    public UserMeta(String name, String pictureURL, int generation) {
         setName(name);
         setPictureURL(pictureURL);
+        setGeneration(generation);
     }
 
     public String getName()         { return name; }
     public String getPictureURL()   { return pictureURL; }
+    public int getGeneration(){return generation;}
 
     public void setName(String name)                { this.name = name; }
     public void setPictureURL(String pictureURL)    { this.pictureURL = pictureURL; }
+    public void setGeneration(int generation)   {this.generation = generation;}
 }

f02762c9f56915dbf744e414483e0540671d4ac6
yang1318 <yang1318@naver.com>
2022-01-06 13:00:09 +0900

    검색 기능 완료

--- app/src/main/java/com/example/chat_de/UserListAdapter.java
+++ app/src/main/java/com/example/chat_de/UserListAdapter.java
@@ -1,5 +1,6 @@
 package com.example.chat_de;
 
+import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,26 +12,27 @@ import android.widget.Filterable;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.chat_de.datas.UserMeta;
+import com.example.chat_de.datas.User;
 
 import java.util.ArrayList;
 
-public class UserListAdapter extends RecyclerView.Adapter<UserListViewHolder> implements Filterable {
-
-    private ArrayList<UserItem> userList;
+public class UserListAdapter extends RecyclerView.Adapter<UserListViewHolder> implements Filterable{
+    Context context;
+    private ArrayList<UserItem> filteredUsers = new ArrayList<>(); //필터링된 리스트 -> 보여줄 리스트
+    private ArrayList<UserItem> unFilteredUsers = new ArrayList<>(); //필터링되지않은 리스트
 
     //뷰홀더가 생성 됐을 때
     @NonNull
     @Override
     public UserListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_item, parent, false);
         return new UserListViewHolder(view);
     }
 
     //아이템 개수를 조회
     @Override
     public int getItemCount() {
-        return this.userList.size();
+        return (this.filteredUsers.size());
     }
 
     public int getItemViewType(int position) {
@@ -40,7 +42,7 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListViewHolder> im
     //뷰와 뷰홀더가 묶였을 때
     @Override
     public void onBindViewHolder(@NonNull UserListViewHolder holder, int position) {
-        final UserItem item = userList.get(position);
+        final UserItem item = filteredUsers.get(position);
 
         holder.checkBox.setOnCheckedChangeListener(null);
 
@@ -55,18 +57,54 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListViewHolder> im
                 item.setChcked(compoundButton.isChecked());
             }
         });
-        holder.bind(this.userList.get(position));
+        holder.bind(this.filteredUsers.get(position));
         //Log.d("TAG","position"+String.valueOf(position));
     }
 
     //외부에서 데이터 넘기기
     public void setUserList(ArrayList<UserItem> userList){
-        this.userList = userList;
+        this.unFilteredUsers = userList;
         notifyDataSetChanged();
+    }
+    //생성자
+    public UserListAdapter(Context context, ArrayList<UserItem> list) {
+        super();
+        this.context = context;
+        this.unFilteredUsers = list;
+        this.filteredUsers = list;
     }
 
     @Override
     public Filter getFilter() {
-        return null;
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString(); //입력된 스트링
+                if(charString.isEmpty()){
+                    //비어있다면 필터링 되지 않은 리스트를 필터링 된 리스트로 사용
+                    filteredUsers = unFilteredUsers;
+                }else{
+                    ArrayList<UserItem> filteringList = new ArrayList<>(); //필터링중인 리스트
+                    for(int i=0;i<unFilteredUsers.size();i++) {
+                        String name = unFilteredUsers.get(i).getName();
+                        if(name.toLowerCase().contains(charString.toLowerCase())){
+                            filteringList.add(unFilteredUsers.get(i));
+                        }
+                    }
+                    filteredUsers = filteringList;
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = filteredUsers;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                //리사이클러뷰 업데이트
+                filteredUsers = (ArrayList<UserItem>)filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+
     }
 }

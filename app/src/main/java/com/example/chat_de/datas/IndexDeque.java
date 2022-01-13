213a22c351dd2b28a378780032ac55cfd94a9847
mokiia <mokiia0325@gmail.com>
2022-01-13 15:21:07 +0900

    IndexDeque 클래스 추가

---
+++ app/src/main/java/com/example/chat_de/datas/IndexDeque.java
@@ -0,0 +1,52 @@
+package com.example.chat_de.datas;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class IndexDeque<T> implements Cloneable {
+    private ArrayList<T> prev;
+    private ArrayList<T> next;
+    
+    public IndexDeque() {
+        prev = new ArrayList<>();
+        next = new ArrayList<>();
+    }
+
+    public void addFirst(T data) {
+        prev.add(data);
+    }
+    public void addLast(T data) {
+        next.add(data);
+    }
+
+    public T get(int position) {
+        if (position < prev.size())
+            return prev.get(prev.size() - position - 1);
+        else
+            return next.get(position - prev.size());
+    }
+    public T getFirst() {
+        return prev.get(prev.size() - 1);
+    }
+    public T getLast() {
+        return next.get(next.size() - 1);
+    }
+    public int getSize() {
+        return prev.size() + next.size();
+    }
+    
+    public void clear() {
+        prev.clear();
+        next.clear();
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
+

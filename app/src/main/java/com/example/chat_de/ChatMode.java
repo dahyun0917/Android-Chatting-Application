a0e0d55ec0b15c214da1bb9d21896704bb034a46
yang1318 <yang1318@naver.com>
2022-01-21 13:27:56 +0900

    관리자 모드 추가, 기수모드 개선 기존에는 기수모드로 넘어가는 Activity가 달랐음, 그런데 한 액티비티에서 받는 것으로 수정. 관리자 모드의 경우 메뉴바에 있던 아이콘을 툴바로 옮기고, 관리자 모드가 아닌 일반 사용자 모드의 경우 대화창 만드는 버튼과 초대하기 버튼이 뜨지 않도록 개선. 그리고 ChatMode 클래스의 경우, 지울까 생각했는데 RoomActivity에서도 현재 모드가 무엇인지 알아야되니까 그대로 두었음.

--- app/src/main/java/com/example/chat_de/ChatMode.java
+++ app/src/main/java/com/example/chat_de/ChatMode.java
@@ -1,10 +1,19 @@
 package com.example.chat_de;
 
 public class ChatMode {
-    private static int CHAT_MODE;
-    public static void generalMode()    { CHAT_MODE = 0; }
-    public static void groupMode()      { CHAT_MODE = 1; } //추후 기수 번호 담기
-    public static int getChatMode() {
-        return CHAT_MODE;
+    private static int chatMode;
+    //public static boolean adminMode;
+
+    public static void setGeneralMode()                     { chatMode = 0; }
+    public static void setGroupMode()                       { chatMode = 1; } //추후 chatMode에 기수 번호 담으면 좋을듯
+    public static int getChatMode()                      { return chatMode; }
+    public static void changeMode() {
+        if(chatMode > 0)
+            chatMode = 0;
+        else
+            chatMode = 1;
     }
+
+    /*public static boolean isAdminMode()                  { return adminMode; }
+    public static void setAdminMode(boolean adminMode)   { ChatMode.adminMode = adminMode; }*/
 }

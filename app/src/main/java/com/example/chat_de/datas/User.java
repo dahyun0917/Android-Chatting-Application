45820c5bf115098f7e72b365e569849dc8886ced
mokiia <mokiia0325@gmail.com>
2022-01-21 10:13:01 +0900

    DB 수정해서 admin 관련 주석 풀었음

--- app/src/main/java/com/example/chat_de/datas/User.java
+++ app/src/main/java/com/example/chat_de/datas/User.java
@@ -10,15 +10,15 @@ public class User implements Serializable {
     protected String pictureURL;
     protected int generation;
     protected String userKey;
-//    protected boolean admin;
+    protected boolean admin;
 
     public User() { }
-    public User(String name, String pictureURL, int generation, String userKey/*, boolean admin*/) {
+    public User(String name, String pictureURL, int generation, String userKey, boolean admin) {
         setName(name);
         setPictureURL(pictureURL);
         setGeneration(generation);
         setUserKey(userKey);
-//        setAdmin(admin);
+        setAdmin(admin);
     }
     //Copy constructor
     public User(User original) {
@@ -32,20 +32,21 @@ public class User implements Serializable {
     public String getPictureURL()   { return pictureURL; }
     public int getGeneration()      { return generation; }
     public String getUserKey()      { return userKey; }
-//    public boolean getAdmin()       { return admin; }
+    public boolean getAdmin()       { return admin; }
     public User userMeta()          { return this; }
 
     public void setName(String name)                { this.name = name; }
     public void setPictureURL(String pictureURL)    { this.pictureURL = pictureURL; }
     public void setGeneration(int generation)       { this.generation = generation; }
     public void setUserKey(String userKey)          { this.userKey = userKey; }
-//    public void setAdmin(boolean admin)             { this.admin = admin; }
+    public void setAdmin(boolean admin)             { this.admin = admin; }
 
     @Override
     public String toString() {
         return  "{ name: " + name +
                 ", pictureURL: " + pictureURL +
                 ", generation: " + generation +
-                ", userKey: " + userKey + " }";
+                ", userKey: " + userKey +
+                ", admin: " + admin + " }";
     }
 }

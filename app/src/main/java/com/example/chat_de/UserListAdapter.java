2b903ca95c1b51d9b5f34b17a669b4a631258d1e
yang1318 <yang1318@naver.com>
2022-01-24 09:52:27 +0900

    UserListActivity1 에서 선택된 유저 보이는 화면 구현 완료

--- app/src/main/java/com/example/chat_de/UserListAdapter.java
+++ app/src/main/java/com/example/chat_de/UserListAdapter.java
@@ -18,6 +18,7 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListViewHolder> im
     Context context;
     private ArrayList<UserListItem> filteredUsers = new ArrayList<>(); //필터링된 리스트 -> 보여줄 리스트
     private ArrayList<UserListItem> unFilteredUsers = new ArrayList<>(); //필터링되지않은 리스트
+    private CheckBoxClickListener listener;
 
     //생성자1
     public UserListAdapter(Context context, ArrayList<UserListItem>[] userList) {
@@ -70,7 +71,13 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListViewHolder> im
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 //체크상태가 바뀌면 item의 checked값도 바뀜.
-                item.setChecked(compoundButton.isChecked());
+                if(compoundButton.isChecked()){
+                    listener.onCheckedClick(item.getUserKey());
+                }
+                else {
+                    item.setChecked(compoundButton.isChecked());
+                    listener.onUnCheckedClick(item.getUserKey());
+                }
             }
         });
         holder.bind(this.filteredUsers.get(position));
@@ -88,6 +95,10 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListViewHolder> im
         for(ArrayList<UserListItem> i : userList)
             list.addAll(i);
         setUserList(list);
+    }
+
+    public void setOnCheckBoxClickListener(CheckBoxClickListener listener){
+        this.listener = listener;
     }
 
     @Override
